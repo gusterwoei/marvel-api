da239b80a7a360fdde21191f0ce0167cb7e394ef
Guster Woei <gusterwoei@gmail.com>
2021-03-20 16:28:47 +0800

    docker integration

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const routes = require('./app/router')
-const port = process.env.PORT || 3000
+const port = 3000
 const env = process.env.NODE_ENV || 'dev'
 
 app.use(express.json())
